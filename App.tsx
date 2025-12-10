236f9579b3abb2df9dcfe428746914740ecbe14e
eloy garofano <eloygarov@gmail.com>
2025-12-10 10:59:39 +0100

    imports App.tsx

--- App.tsx
+++ App.tsx
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native';
+import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
+import { useState } from 'react';
+
 
 export default function App() {
   return (
